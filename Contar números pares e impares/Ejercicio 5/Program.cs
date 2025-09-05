7924f2cb540e6defad123e9bfe537d2c03bcc307
Kenneth Zamora <kmzamora@uamv.edu.ni>
2025-09-05 09:57:44 -0600

    Ejercicio Kenneth Zamora

---
+++ Contar números pares e impares/Ejercicio 5/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Contar numeros pares e impares");
+Console.WriteLine("Ingresa la cantidad de numeros a procesar:");
+int cantidad = int.Parse(Console.ReadLine()!);
+int pares = 0;
+int impares = 0;
+
+for (int i = 0; i < cantidad; i++)
+{
+    Console.WriteLine($"Ingresa el número #{i + 1}:");
+    int numero = int.Parse(Console.ReadLine()!);
+    if (numero % 2 != 0)
+    {
+        impares++;
+    }
+    else
+    {
+        pares++;
+    }
+}
+
+Console.WriteLine($"Cantidad de números pares: {pares}");
+Console.WriteLine($"Cantidad de números impares: {impares}");
