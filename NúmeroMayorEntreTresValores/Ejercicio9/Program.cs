9d1fa2a9e8a84006b682c6e3d67bedda19571421
Jeremy Gradiz <jjgradiz@uamv.edu.ni>
2025-09-05 10:25:56 -0600

    Jeremy Gradiz

---
+++ NúmeroMayorEntreTresValores/Ejercicio9/Program.cs
@@ -0,0 +1,33 @@
+﻿//Solicita tres números diferentes al usuario y
+//usa estructuras if para determinar cuál es el mayor.
+
+namespace Ejercicio9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                Console.WriteLine("Ingrese el primer número:");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundo número:");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el tercer número:");
+                int num3 = int.Parse(Console.ReadLine());
+                if (num1 > num2 && num1 > num3)
+                {
+                    Console.WriteLine($"El número mayor es: {num1}");
+                }
+                else if (num2 > num1 && num2 > num3)
+                {
+                    Console.WriteLine($"El número mayor es: {num2}");
+                }
+                else
+                {
+                    Console.WriteLine($"El número mayor es: {num3}");
+                }
+        }
+    }
+}
+
---
+++ ValidaciónDeContraseña/Ejercicio8/Program.cs
@@ -0,0 +1,19 @@
+﻿//Crea un programa que pida al usuario una contraseña
+//y repita la solicitud hasta que ingrese la correcta.
+//Usa un bucle do while.
+
+
+
+namespace Ejercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Ingrese la contraseña:");
+            } while (Console.ReadLine() != "redwood");
+        }
+    }
+}
