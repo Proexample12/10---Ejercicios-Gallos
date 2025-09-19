84520b3a05f850d582af2524e195fcb8fe3a5940
Maverick Miranda <maverickmg@uamv.edu.ni>
2025-09-19 09:12:18 -0600

    Ejericicio Maverick Miranda

---
+++ TablasDeMultiplicar/Ejericicio4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ejericicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, multiplo, producto;
+            multiplo = 1;
+           
+            Console.Write("Digite un numero entero: ");
+            num=int.Parse(Console.ReadLine()!);
+            do
+            {
+                producto = num * multiplo;
+                Console.WriteLine(num + " x " + multiplo + "= "+ producto);
+                multiplo = multiplo + 1;
+            }while(multiplo < 11);
+            
+        }
+    }
+}
